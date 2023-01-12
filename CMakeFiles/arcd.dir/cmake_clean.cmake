c68db5fc0856e0148017fd51e44355550105c8dc
Ting-yuChen <chen.beans@gmail.com>
2023-01-12 23:30:17 +0800

    Initial commit

---
+++ CMakeFiles/arcd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arcd.dir/arcd.c.o"
+  "CMakeFiles/arcd.dir/arcd.c.o.d"
+  "libarcd.a"
+  "libarcd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arcd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ arcd/CMakeFiles/arcd.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for arcd.
+# This may be replaced when dependencies are built.
